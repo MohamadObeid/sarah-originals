5599c1f674d981228f44eed83027fd36e46fbbdc
MohamadObeid <komikbenim@gmail.com>
2020-12-25 18:01:42 +0200

    commit 25 dec

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL, //|| 'mongodb://localhost/sarahoriginals',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/sarahoriginals',
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecrets ",
   accessKeyId: process.env.accessKeyId || 'AKIAURMZYOA53B55C46S',
   secretAccessKey: process.env.secretAccessKey || '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',
